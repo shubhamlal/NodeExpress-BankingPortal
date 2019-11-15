0933c5aaee6ee877fd2a679f306dfd2aebd87276
Lal <SXL0DFB@fpl.com>
2019-11-15 12:38:42 -0500

    2nd Module

--- src/app.js
+++ src/app.js
@@ -4,10 +4,35 @@ const express = require('express')
 
 const app = express();
 
+
+
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/', (req,res) => res.render('index',{title:'Index'}));
+/////////////////////Accounts Data/////////////////////
+const accountData = fs.readFileSync(
+    path.join(__dirname,'json','accounts.json'),'utf8');
+const accounts = JSON.parse(accountData);
+
+//////////////////User Data//////////////////////////
+
+const userData = fs.readFileSync(
+    path.join(__dirname,'json','users.json'), 'utf8'
+);
+const user = JSON.parse(userData);
+
+app.get('/', (req,res) => 
+    res.render('index',{title:'AccountsSummary', accounts }));
+
+app.get('/savings', (req,res) => {
+    res.render('account',{account:accounts.savings});
+});
+app.get('/checking', (req,res) => {
+    res.render('account',{account:accounts.checking});
+});
+app.get('/credits', (req,res) => {
+    res.render('account',{account:accounts.credit});
+});
 
 app.listen(3000, ()=> console.log('PS Project running on port 3000!'))
