5e593e524849dd795d1d41410c0dbe26fb5daa3e
Shubham Lal <shubhamlal400@gmail.com>
2019-11-17 14:02:43 +0530

    3rd module

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use(express.urlencoded({extended:true}))
+
 /////////////////////Accounts Data/////////////////////
 const accountData = fs.readFileSync(
     path.join(__dirname,'json','accounts.json'), 'utf8');
@@ -38,5 +40,37 @@ app.get('/credit', (req,res) => {
 app.get('/profile', (req,res) => {
     res.render('profile',{user:users[0]})
 });
+
+////////////////////// TRANSFER FEATURE //////////////////////////////////////////////
+
+app.get('/transfer', (req,res) => 
+    res.render('transfer'));
+
+app.post('/transfer', (req,res) => {
+
+    console.log("Amount: "+req.body.amount)
+    accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount)
+    console.log(accounts[req.body.from].balance);
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10)
+
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    res.render('transfer', {message: 'Transfer Completed' });
+});
+
+
+/////////////////////////// PAYMENT FEATURE //////////////////////////////////
+
+app.get('/payment', (req,res) => {
+    res.render('payment',{account:accounts.credit})
+});
+
+app.post('/payment', (req,res) => {
+    accounts.credit.balance = accounts.credit.balance - parseInt(req.body.amount);
+    accounts.credit.available = accounts.credit.available + parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    res.render('payment', { message: "Payment Successful", account: accounts.credit });
+})
 
 app.listen(3000, ()=> console.log('PS Project running on port 3000!'))
