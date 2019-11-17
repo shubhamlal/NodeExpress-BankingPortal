739040ff81d6b7e7b05a92f7bf7c78b3b83c3928
Shubham Lal <shubhamlal400@gmail.com>
2019-11-17 17:11:49 +0530

    Module3

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 
+const {accounts,users,writeJSON} = require('./data.js')
+
 const app = express();
 
 
@@ -12,17 +14,6 @@ app.use(express.static(path.join(__dirname,'public')))
 
 app.use(express.urlencoded({extended: true}));
 
-/////////////////////Accounts Data/////////////////////
-const accountData = fs.readFileSync(
-    path.join(__dirname,'json','accounts.json'), 'utf8');
-const accounts = JSON.parse(accountData);
-
-//////////////////User Data//////////////////////////
-
-const userData = fs.readFileSync(
-    path.join(__dirname,'json','users.json'), 'utf8'
-);
-const users = JSON.parse(userData);
 
 app.get('/', (req,res) => 
     res.render('index',{ title: 'Account Summary', accounts }));
@@ -51,8 +42,7 @@ app.post('/transfer', (req,res) => {
     accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount)
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10)
 
-    const accountsJSON = JSON.stringify(accounts,null,4);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed' });
 });
 
@@ -66,8 +56,7 @@ app.get('/payment', (req,res) => {
 app.post('/payment', (req,res) => {
     accounts.credit.balance = accounts.credit.balance - parseInt(req.body.amount);
     accounts.credit.available = accounts.credit.available + parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts,null,4);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    writeJSON();
     res.render('payment', { message: "Payment Successful", account: accounts.credit });
 })
 
---
+++ src/data.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+
+/////////Accounts///////////////////////////////////////////////////////////////////
+
+////////READ//////////////////
+
+const accountData = fs.readFileSync(
+    path.join(__dirname,'json','accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+
+
+/////////////////////USER DATA////////////////////////////////////////////////////
+
+//////////READ/////////
+const userData = fs.readFileSync(
+    path.join(__dirname,'json','users.json'), 'utf8'
+);
+const users = JSON.parse(userData);
+
+
+
+
+/////METHODS/////////////////////////////
+
+const writeJSON = () => {
+    const accountsJSON = JSON.stringify(accounts,null,4);
+fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+}
+
+module.exports = [accounts,users,writeJSON]
